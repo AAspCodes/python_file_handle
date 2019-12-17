a12aea87e3fe631422780903966f3ff9d1fbe5fe
SCC-CS-Engineering-Club <58450710+SCC-CS-Engineering-Club@users.noreply.github.com>
2019-12-16 19:26:39 -0800

    add pyfile

---
+++ main.py
@@ -0,0 +1,22 @@
+def read_from_file():
+    with open("text_file", "r") as f:
+        contents = f.read()
+        
+    return contents
+
+
+
+def write_to_file(text):
+    with open("text_file", "w") as f:
+        f.write(text)
+        
+
+def append_file(text):
+    with open("text_file", "a+") as f:
+        f.append(text)
+
+        
+write_to_file("hello world")
+print(read_from_file())
+
+append_file("heelo again")
