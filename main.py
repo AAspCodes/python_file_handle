9e51549463f5a0f2d6084c2a959ab5a9ed045116
Anthony Asp <asp.anthony1@gmail.com>
2019-12-30 22:21:54 -0800

    add more excitement

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def append_file(text):
 # option 2
 """Using 'with open' automatically closes the file when indentation
  is in line with the 'with' keyword.
- Using 'with open' also makes code less verbose."""
+ Using 'with open' also makes code less verbose!!!!"""
 
 
 def with_read_from_file():
