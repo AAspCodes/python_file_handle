b9f8a6828512832963614c955317f98e7342bd41
Anthony Asp <asp.anthony1@gmail.com>
2019-12-30 22:06:49 -0800

    add non-'with' forms of file handleing to show options
    + add read, write, and append, functions with out the use of the 'with' keyword to show more variety in file handleing.
    
    + Add comments to explain the advatange of the 'with' keyword'.

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 """Some examples of how to read, write and append a file. """
 
 
+# option 1
 def read_from_file():
+    f = open("text_file", "r")
+    contents = f.read()
+    f.close()
+    return contents
+
+
+def write_to_file(text):
+    f = open("text_file", "w")
+    f.write(text)
+    f.close()
+
+
+def append_file(text):
+    f = open("text_file", "a")
+    f.write(text)
+    f.close()
+
+
+# option 2
+"""Using 'with open' automatically closes the file when indentation
+ is in line with the 'with' keyword."""
+
+
 def with_read_from_file():
     with open("text_file", "r") as f:
         contents = f.read()
