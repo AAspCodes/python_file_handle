0bfffc22ccf084e487ae05dba9a54191e1c2ca0e
Anthony Asp <asp.anthony1@gmail.com>
2019-12-30 22:04:31 -0800

    add 'with' infront of function name to show style
    ~ add "with_" infront of the function names to show that with was used inside and show contrast to other functions coming in the future

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+"""Some examples of how to read, write and append a file. """
+
+
 def read_from_file():
+def with_read_from_file():
     with open("text_file", "r") as f:
         contents = f.read()
     return contents
 
 
-
-def write_to_file(text):
+def with_write_to_file(text):
     with open("text_file", "w") as f:
         f.write(text)
-        
 
-def append_file(text):
+
+def with_append_file(text):
     with open("text_file", "a") as f:
         f.write(text)
-
-        
-
